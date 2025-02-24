0c69b0f30b4ff9af3ab6ec939d1d3d5f300ba44b
MihirGajjar27 <165854969+MihirGajjar27@users.noreply.github.com>
2025-02-23 17:45:44 -0800

    updated border styling

--- components/Header.jsx
+++ components/Header.jsx
@@ -11,7 +11,7 @@ export default function Header({ isDarkMode, setIsDarkMode }) {
             className={`
             sticky top-0 z-50 h-15 w-full
             border-b border-solid 
-            ${isDarkMode ? "bg-gray-1000 border-gray-700" : "bg-white border-gray-300"}
+            ${isDarkMode ? "bg-gray-1000 border-b-gray-700 border-x-gray-1000 border-t-gray-1000" : "bg-white border-b-gray-300 border-x-white border-t-white"}
           `}
         >
         <div className={"flex justify-center"}>
