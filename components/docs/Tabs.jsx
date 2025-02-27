4b3bc7dbfd2a99a9b76ef31763c89cf00e8f427d
MihirGajjar27 <165854969+MihirGajjar27@users.noreply.github.com>
2025-02-26 21:46:54 -0800

    updated border styling

--- components/docs/Tabs.jsx
+++ components/docs/Tabs.jsx
@@ -7,21 +7,22 @@ export default function Tabs({ children, isDark = false, tabs = [] }) {
 
     return (
         <div>
-            <div className={`flex space-x-4 border-b-1 border-solid border-x-transparent border-t-transparent ${lineColor}`}>
+            <div className={`flex space-x-4 border-b-1 border-x-transparent border-t-transparent border-solid ${lineColor}`}>
                 {tabs.map((tab, idx) => {
                     const isActive = idx === activeTab;
                     return (
-                        <button
-                            key={tab}
-                            onClick={() => setActiveTab(idx)}
-                            className={`pb-3 px-2 text-sm sm:text-base ${
-                                isActive
-                                    ? `border-b-2 border-x-transparent border-t-transparent border-solid border-current`
-                                    : ``
-                            }`}
-                        >
-                            {tab}
-                        </button>
+                        <div className={`pb-3 px-2 text-sm sm:text-base ${
+                            isActive
+                                ? `border-b-2 border-x-transparent border-t-transparent border-solid border-current`
+                                : ``
+                        }`}>
+                            <button
+                                key={tab}
+                                onClick={() => setActiveTab(idx)}
+                            >
+                                {tab}
+                            </button>
+                        </div>
                     );
                 })}
             </div>
